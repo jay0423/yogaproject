f473f897e62555b63150f7280e9663abe653b319
jay0423 <jay0423@i.softbank.jp>
2021-01-23 13:32:14 +0900

    ANALYSISクラスを作成し，分析画面のコードを整理．

--- yogaapp/views/view5.py
+++ yogaapp/views/view5.py
@@ -62,73 +62,68 @@ def users_update(request, username):
     return render(request, 'users_update.html', context)
 
 
-#売上やピボットテーブル
-@login_required
-def analysis_func(request):
-    #DataFrameの取得
-    from django_pandas.io import read_frame
-    plan_model = PlanModel.objects.all()
-    if list(plan_model) == []:
-        context = {
-            'post': False,
-            'error1': 'データがありません．',
-            'month_list': [],
-            'from_month': '',
-            'to_month': '',
-        }
-        return render(request, 'analysis.html', context)
-    columns = [item.name for item in PlanModel._meta.get_fields()]
-    df = read_frame(plan_model, fieldnames=columns)
-    df = df.sort_values(['date', 'time'])
-    df.reset_index(inplace=True)
-    df = df.drop('index', axis=1)
-    #日付の最小値と最大値の選定
-    min_month = df.iloc[0, :].month
-    max_month = df.iloc[-1, :].month
-    month_list = sorted(list(set(df.month)))
-    error1 = ''
-    #デフォルト値
-    index = 'weekday'
-    columns = 'plan'
-    data = 'rate_of_book'
-    aggfunc = 'mean'
-    columns2 = 'plan'
-    #入力された時の処理
-    if request.method == "POST": 
-        post = True
-        from_month = request.POST['from_month']
-        to_month = request.POST['to_month']
+class ANALYSIS:
+        
+    def __init__(self, post):
+        self.post = post
+        self.error1 = ""
+        self.from_month = ""
+        self.last_month = ""
+        self.index = 'weekday'
+        self.columns = 'plan'
+        self.columns2 = "plan"
+        self.data = 'rate_of_book'
+        self.aggfunc = 'mean'
+
+    def plus_zero(self, num): #1ケタの月の先頭に0を足す
+        return '0' + str(num) if len(str(num)) == 1 else str(num)
+    
+    def make_month_list(self, first_month, last_month):
+        first_year = int(first_month[:4])
+        last_year = int(last_month[:4])
+        month_list = []
+        for year in range(first_year, last_year+1):
+            for month in range(1, 13):
+                month_list.append(str(year) + "-" + str(self.plus_zero(month)))
+        month_list = month_list[month_list.index(first_month): month_list.index(last_month)+1]
+        return month_list
+    
+    def make_first_last_month(self):
+        first_month = PlanModel.objects.first().month
+        last_month = PlanModel.objects.last().month
+        return first_month, last_month
+    
+    def get_post(self, request):
+        self.post = True
+        self.from_month = request.POST['from_month']
+        self.to_month = request.POST['to_month']
         try:
-            index = request.POST['index']
-            columns2 = request.POST['columns']
-            data = request.POST['data']
-            aggfunc = request.POST['aggfunc']
+            self.index = request.POST['index']
+            self.columns2 = request.POST['columns']
+            self.data = request.POST['data']
+            self.aggfunc = request.POST['aggfunc']
+            self.columns = 'weekday' if self.columns2 == 'weekday2' else self.columns2
         except:
             pass
-        columns = 'weekday' if columns2 == 'weekday2' else columns2
-        
-        #dfの編集
-        from_month_num = list(df.query('month == "{}"'.format(from_month)).index)[0]
-        to_month_num = 1 + list(df.query('month == "{}"'.format(to_month)).index)[-1]
-        if from_month_num >= to_month_num:
-            context = {
-                'post': False,
-                'error1': '期間が矛盾しています．',
-                'month_list': month_list,
-                'from_month': '',
-                'to_month': '',
-            }
-            return render(request, 'analysis.html', context)
-        df = df.iloc[from_month_num: to_month_num]
-        if list(df[df.loc[:, 'booked_people_name'] != ''].loc[:, 'booked_people_name']) == []:
-            context = {
-                'post': False,
-                'error1': '予約者が一人もいません．',
-                'month_list': month_list,
-                'from_month': '',
-                'to_month': '',
-            }
-            return render(request, 'analysis.html', context)
+    
+    #以降データ分析用の処理
+    def make_df(self):
+        #DataFrameの作成
+        month_list = self.make_month_list(self.from_month, self.to_month)
+        columns_list = [item.name for item in PlanModel._meta.get_fields()]
+        try:
+            df = read_frame(PlanModel.objects.filter(month=month_list[0]), fieldnames=columns_list)
+        except:
+            self.error1 = "error"
+            return
+        for month in month_list[1:]:
+            df = df.append(read_frame(PlanModel.objects.filter(month=month), fieldnames=columns_list))
+        df = df.sort_values(['date', 'time'])
+        df.reset_index(inplace=True)
+        df = df.drop('index', axis=1)
+        return df
+    
+    def add_columns(self, df):
         #曜日columnの追加
         weekday_d = {0:'月', 1:'火', 2:'水', 3:'木', 4:'金', 5:'土', 6:'日'}
         format = lambda x: weekday_d[x.weekday()]
@@ -143,22 +138,15 @@ def analysis_func(request):
         #売上columnの追加
         format = lambda x: x['price'] * x['number_of_people']
         df.loc[:, 'total_price'] = df.apply(format, axis=1)
+        return df
+    
+    def make_total_price(self, df):
         #月ごとの売上金額の集計
-        total_price_per_month = df.groupby('month')[['number_of_people', 'total_price']].sum()
-        #pivot_tableの作成
-        pivot_table = df.pivot_table(data, index=index, columns=columns, aggfunc=aggfunc, margins=True)
-        #順番の整理用
-        if index == 'weekday':
-            pivot_table = pivot_table.reindex(index=['月', '火', '水', '木', '金', '土', 'All'])
-        if columns == 'weekday':
-            columns_list = ['月', '火', '水', '木', '金', '土', 'All']
-        elif columns == 'plan':
-            columns_list = list(pivot_table.columns)[:-1]
-            columns_num_list = []
-            for plan in columns_list:
-                columns_num_list.append(SettingPlanModel.objects.get(name=plan).plan_num)
-            columns_list = [item for _, item in sorted(zip(columns_num_list, columns_list))] + ['All'] 
-        pivot_table = pivot_table.reindex(columns=columns_list)
+        total_price_per_month = df.groupby("month")[["number_of_people"]].sum()
+        total_price_per_month["total_price"] = df.groupby("month")[["total_price"]].sum()
+        return total_price_per_month
+    
+    def round_func(self, pivot_table):
         #各要素を四捨五入する
         def round_func1(x):
             try:
@@ -170,24 +158,46 @@ def analysis_func(request):
                 return str(int(x))
             except:
                 return None
-        if aggfunc == 'mean' or aggfunc == 'std' or aggfunc == 'median':
-            if data == 'total_price':
+        if self.aggfunc == 'mean' or self.aggfunc == 'std' or self.aggfunc == 'median':
+            if self.data == 'total_price':
                 pivot_table = pivot_table.applymap(round_func2).dropna(axis=1, how='all').dropna(how='all').fillna('')
             else:
                 pivot_table = pivot_table.applymap(round_func1).dropna(axis=1, how='all').dropna(how='all').fillna('')
-        elif aggfunc == 'sum':
+        elif self.aggfunc == 'sum':
             pivot_table = pivot_table.applymap(round_func2).dropna(axis=1, how='all').dropna(how='all').fillna('')
-        columns_list = list(pivot_table.columns)
+        return pivot_table
+        
+    def make_pivot_table(self, df):
+        #pivot_tableの作成
+        pivot_table = df.pivot_table(self.data, index=self.index, columns=self.columns, aggfunc=self.aggfunc, margins=True)
+        #順番の整理用
+        if self.index == 'weekday':
+            pivot_table = pivot_table.reindex(index=['月', '火', '水', '木', '金', '土', 'All'])
+        if self.columns == 'weekday':
+            columns_list = ['月', '火', '水', '木', '金', '土', 'All']
+        elif self.columns == 'plan':
+            columns_list = list(pivot_table.columns)[:-1]
+            columns_num_list = []
+            for plan in columns_list:
+                columns_num_list.append(SettingPlanModel.objects.get(name=plan).plan_num)
+            columns_list = [item for _, item in sorted(zip(columns_num_list, columns_list))] + ['All'] 
+        pivot_table = pivot_table.reindex(columns=columns_list)
+        pivot_table = self.round_func(pivot_table)
+        return pivot_table
+    
+    def make_terms(self):
         #表示する条件
         en_dict = {'weekday': '曜日', 'time': '時間', 'plan': 'プラン', 'rate_of_book': '予約率', 'number_of_people': '予約数', 'total_price': '売上金額', 'mean': '平均値', 'sum': '合計', 'std': '標準偏差', 'median': '中央値'}
-        if data == 'rate_of_book':
+        if self.data == 'rate_of_book':
             unit = '%'
-        elif data == 'number_of_people':
+        elif self.data == 'number_of_people':
             unit = '-'
-        elif data == 'total_price':
+        elif self.data == 'total_price':
             unit = '円'
-        terms = '{}の{}【{}】｜{}－{}'.format(en_dict[data], en_dict[aggfunc], unit, en_dict[index], en_dict[columns])
-
+        terms = '{}の{}【{}】｜{}－{}'.format(en_dict[self.data], en_dict[self.aggfunc], unit, en_dict[self.index], en_dict[self.columns])
+        return terms
+    
+    def make_df_to_csv(self, df):
         #名前を分割して行を追加する処理
         df.reset_index(inplace=True)
         df.drop('index', axis=1, inplace=True)
@@ -209,24 +219,35 @@ def analysis_func(request):
         df_to_csv.insert(0, 'new_id', df_to_csv.loc[:, 'date'].map(str) + df_to_csv.loc[:, 'plan'] + df_to_csv.loc[:, 'time'] + df_to_csv.loc[:, 'booked_people_name'])
         df_to_csv.loc[:, 'booked_people'] = df_to_csv.loc[:, 'booked_people'].map(lambda x: x.replace(' ', ''))
         df_to_csv.loc[:, 'booked_people_name'] = df_to_csv.loc[:, 'booked_people_name'].map(lambda x: x.replace(' ', ''))
+        return df_to_csv
+    
+    def make_rank_book(self, df_to_csv):
         #予約者の頻度ランキング
-        booked_people_pivot_table = df_to_csv[df_to_csv.loc[:, 'booked_people_name'] != ''].pivot_table(data, index='booked_people_name', columns='plan', aggfunc='count', fill_value=0, margins=True)
+        booked_people_pivot_table = df_to_csv[df_to_csv.loc[:, 'booked_people_name'] != ''].pivot_table(self.data, index='booked_people_name', columns='plan', aggfunc='count', fill_value=0, margins=True)
         columns_list2 = list(booked_people_pivot_table.columns)
         pivot_list = [booked_people_pivot_table[:-1].loc[:, columns].sort_values(ascending=False) for columns in columns_list2]
         rank2 = [i + 1 for i in range(len(booked_people_pivot_table[:-1].values))]
         pivot_list = [zip(rank2, list(item.index), item.values) for item in pivot_list]
-        
-        context = {        
-            'post': post,
-            'error1': error1,
+        return columns_list2, pivot_list
+
+    def get_context_data(self):
+        df = self.add_columns(self.make_df())
+        total_price_per_month = self.make_total_price(df)
+        pivot_table = self.make_pivot_table(df)
+        columns_list = list(pivot_table.columns)
+        terms = self.make_terms()
+        df_to_csv = self.make_df_to_csv(df)
+        (columns_list2, pivot_list) = self.make_rank_book(df_to_csv)
+        context = {
+            'post': self.post,
+            'error1': self.error1,
             'terms': terms,
-            'from_month': from_month,
-            'to_month': to_month,
-            'index': index,
-            'columns': columns2,
-            'data': data,
-            'aggfunc': aggfunc,
-            'month_list': month_list,
+            'from_month': self.from_month,
+            'to_month': self.to_month,
+            'index': self.index,
+            'columns': self.columns2,
+            'data': self.data,
+            'aggfunc': self.aggfunc,
             'total_price_per_month': total_price_per_month,
             'pivot_table': pivot_table,
             'columns_list': columns_list,
@@ -234,18 +255,68 @@ def analysis_func(request):
             'columns_list2': columns_list2,
             'len_columns_list2': len(columns_list2),
         }
-        return render(request, 'analysis.html', context)
-    
-    context = {
-        'post': False,
-        'error1': '',
-        'month_list': month_list,
-        'from_month': '',
-        'to_month': '',
-    }
-    return render(request, 'analysis.html', context)
+        return context
         
     
+#売上やピボットテーブル
+@login_required
+def analysis_func(request):
+    #入力された時の処理
+    if request.method == "POST": 
+        post = True
+        a = ANALYSIS(post)
+        a.get_post(request)
+        # try:
+        (first_month, last_month) = a.make_first_last_month()
+        month_list = a.make_month_list(first_month, last_month)
+        # except:
+        #     context = {
+        #         'post': False,
+        #         'error1': 'データがありません．',
+        #         'month_list': [],
+        #         'from_month': '',
+        #         'to_month': '',
+        #     }
+        #     return render(request, 'analysis.html', context)
+        df = a.make_df()
+        if a.error1 == "error": #エラー時
+            context = {
+                'post': False,
+                'error1': '期間が矛盾しています．',
+                'month_list': month_list,
+                'from_month': '',
+                'to_month': '',
+            }
+            return render(request, 'analysis.html', context)
+        try:
+            context = a.get_context_data()
+        except: #指定月にデータがないとき
+            context = {
+                'post': False,
+                'error1': '予約者が一人もいません．',
+                'month_list': month_list,
+                'from_month': '',
+                'to_month': '',
+            }
+            return render(request, 'analysis.html', context)
+            
+        context["month_list"] = month_list
+        return render(request, 'analysis.html', context)
+    else:
+        post = False
+        a = ANALYSIS(post)
+        (first_month, last_month) = a.make_first_last_month()
+        month_list = a.make_month_list(first_month, last_month)
+        context = {
+            'post': False,
+            'error1': '',
+            'month_list': month_list,
+            'from_month': '',
+            'to_month': '',
+        }
+    return render(request, 'analysis.html', context)
+        
+
 #テーブル
 @login_required
 def table_func(request):
