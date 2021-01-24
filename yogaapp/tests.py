f6cc97dc2c357aca086c04f4735c9a0fe2dd8ebf
jay0423 <jay0423@i.softbank.jp>
2021-01-24 14:57:20 +0900

    testコードの追加．

--- yogaapp/tests.py
+++ yogaapp/tests.py
@@ -1,3 +1,52 @@
 from django.test import TestCase
+from yogaapp.models import BookModel, PlanModel
+import datetime
+ 
+class PersonModelTests(TestCase):
+    def test_saving_and_retrieving_book_model(self):
+        book = BookModel()
+        user, plan, time_of_cancel = "ゲスト", "プラン", 1
+        book.user = user
+        book.plan = plan
+        book.time_of_cancel = time_of_cancel
+        book.save()
 
-# Create your tests here.
+        saved_book = BookModel.objects.all()
+        actual_book = saved_book[0]
+
+        self.assertEqual(actual_book.user, user)
+        self.assertEqual(actual_book.plan, plan)
+        self.assertEqual(actual_book.time_of_cancel, time_of_cancel)
+    
+    def test_saving_and_retrieving_plan_model(self):
+        plan_model = PlanModel()
+        s = (datetime.date(2020,1,1), "2020-01", "プラン", "プ", 1, "guest", "ゲスト", "09:00", "スタジオ", 10, 1)
+        date, month, plan, short_plan_name, number_of_people, booked_people, booked_people_name, time, location, max_book, plan_num = s
+        plan_model.date = date
+        plan_model.month = month
+        plan_model.plan = plan
+        plan_model.short_plan_name = short_plan_name
+        plan_model.number_of_people = number_of_people
+        plan_model.booked_people = booked_people
+        plan_model.booked_people_name = booked_people_name
+        plan_model.time = time
+        plan_model.location = location
+        plan_model.max_book = max_book
+        plan_model.plan_num = plan_num
+        plan_model.save()
+
+        saved_plan = PlanModel.objects.all()
+        actual_plan = saved_plan[0]
+
+        self.assertEqual(actual_plan.date, date)
+        self.assertEqual(actual_plan.month, month)
+        self.assertEqual(actual_plan.plan, plan)
+        self.assertEqual(actual_plan.short_plan_name, short_plan_name)
+        self.assertEqual(actual_plan.number_of_people, number_of_people)
+        self.assertEqual(actual_plan.booked_people, booked_people)
+        self.assertEqual(actual_plan.booked_people_name, booked_people_name)
+        self.assertEqual(actual_plan.time, time)
+        self.assertEqual(actual_plan.location, location)
+        self.assertEqual(actual_plan.max_book, max_book)
+        self.assertEqual(actual_plan.plan_num, plan_num)
+    
